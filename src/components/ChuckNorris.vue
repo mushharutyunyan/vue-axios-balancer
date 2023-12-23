d42e2615b00e0e66fffc768e561fe1b16416771e
Mush harutyunyan <mush.harutyunyan@hotmail.com>
2023-12-23 20:20:12 +0400

    change search input placeholder

--- src/components/ChuckNorris.vue
+++ src/components/ChuckNorris.vue
@@ -21,7 +21,7 @@
         <div>
           <input type="text" v-model="search"
                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                 placeholder="Search">
+                 placeholder="Search (min. 3 symbols)">
         </div>
         <div v-if="searchData.total && searchData.total > 0">
           <div v-for="row in searchData.result" class="p-2 text-center rounded-md border-2">
